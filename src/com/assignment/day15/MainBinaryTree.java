c61010b1a96e1c0d1b31f1d98dedf9874ecfcf2d
Subhashree naik <subhashree.naik@gmail.com>
2022-03-06 19:28:21 +0530

    Insert Leaf To Binary Tree

--- src/com/assignment/day15/MainBinaryTree.java
+++ src/com/assignment/day15/MainBinaryTree.java
@@ -1,21 +1,82 @@
 package com.assignment.day15;
-
-
-
 public class MainBinaryTree <K extends Comparable<K>> {
 	
      Node<K> root;
+     
      MainBinaryTree()
       {
     	 this.root=null;
       }
+     
+     /*This method is a recursive function to insert a new key in binary search tree
+      * First it will check whether tree is empty or not.If it is empty,then it will return
+      * new node.Otherwise it will recur down to tree.
+      */
+ 	
+     public Node<K> insert(Node<K> root,K x) 
+ 	 {
+ 		if (root == null)
+ 		{
+         return new Node<>(x);
+ 		}
+ 		int y = x.compareTo(root.key);
+ 		if (y == 0)
+ 	       return root;
+ 	    if (y < 0)
+ 	        {
+ 	     	root.left = this.insert(root.left, x);
+ 	        } 
+        else
+        {
+ 	      	root.right = this.insert(root.right, x);
+ 	    }
+ 	    return root;
+ 	}
+     
+     
+     /*This method print the binary tree in inorder manner */
+     
+    void inorderPrint(Node<K> root)
+	  {
+	        if (root != null)
+	        {
+	        	inorderPrint(root.left);
+	            System.out.print(root.key+"  ");
+	            inorderPrint(root.right);
+	        }
+	  }
+    
+    /*Add method is calling the insert method */ 
+    void add(K key)
+ 	  {
+ 	     this.root = this.insert(root, key);
+ 	  }
+    
+    /*print method is calling the inorderPrint method */ 
+ 	void print() 
+ 	{ 
+ 		this.inorderPrint(root); 
+ 	}
 	
 	public static void main(String[] args) {
 		/**
 		* Main method for manipulation Binary search tree
 		*
 		*/
-        System.out.println("Welcome to Binary Search Tree program");   
+		/*
+		* PROCEDURE
+		* 1.Adding leaf to the binary tree
+		* 2.Printing the binary tree in inorder manner
+		*/
+		
+	MainBinaryTree<Integer> tree =new MainBinaryTree<Integer>();
+	       tree.add(56);
+	       tree.add(30);
+	       tree.add(70);
+	       tree.add(80);
+	       tree.add(15);
+	       tree.print();
+           
 	}
 
 }
--- src/com/assignment/day15/Node.java
+++ src/com/assignment/day15/Node.java
@@ -1,6 +1,10 @@
 package com.assignment.day15;
 
 public class Node<K extends Comparable<K>> {
+	
+/*  Class containing left
+    and right child of current node
+    and key value*/	
  K key;
  Node<K> left,right;
  public Node(K x) {
