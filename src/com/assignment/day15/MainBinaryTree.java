08c1be670da048e1595abd555d7c56c9abc4ca33
Subhashree naik <subhashree.naik@gmail.com>
2022-03-06 09:39:54 +0530

    Welcome to Binary Search Tree

---
+++ src/com/assignment/day15/MainBinaryTree.java
@@ -0,0 +1,21 @@
+package com.assignment.day15;
+
+
+
+public class MainBinaryTree <K extends Comparable<K>> {
+	
+     Node<K> root;
+     MainBinaryTree()
+      {
+    	 this.root=null;
+      }
+	
+	public static void main(String[] args) {
+		/**
+		* Main method for manipulation Binary search tree
+		*
+		*/
+        System.out.println("Welcome to Binary Search Tree program");   
+	}
+
+}
---
+++ src/com/assignment/day15/Node.java
@@ -0,0 +1,11 @@
+package com.assignment.day15;
+
+public class Node<K extends Comparable<K>> {
+ K key;
+ Node<K> left,right;
+ public Node(K x) {
+	 this.key=x;
+ }
+ 
+ 
+}
